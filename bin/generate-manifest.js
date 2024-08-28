d639b6aa97b042fb558abb88e7e99dde0c7667b4
Adrien Villalonga <nd4yza9cgmdx4cxpq4box6tv7nh80zgc@proton.me>
2024-08-28 11:04:59 +0200

    build: add nodemon & deploy asset command

--- bin/generate-manifest.js
+++ bin/generate-manifest.js
@@ -4,26 +4,25 @@ import fs from 'node:fs';
 import ManifestConfiguration from '../configuration/manifest.config.js';
 
 const rootDirectory = path.dirname(path.dirname(url.fileURLToPath(import.meta.url)));
-const templateDirectory = path.join(rootDirectory, "configuration", "template");
-const distDirectory = path.join(rootDirectory, "dist");
-
+const templateDirectory = path.join(rootDirectory, 'configuration', 'template');
+const distDirectory = path.join(rootDirectory, 'dist');
 
 const manifestArray = [
     {
-        name: "chrome",
-        entry: path.join(templateDirectory, "chrome.manifest.json"),
-        output: path.join(distDirectory, "chrome", "manifest.json")
+        name: 'chrome',
+        entry: path.join(templateDirectory, 'chrome.manifest.json'),
+        output: path.join(distDirectory, 'chrome', 'manifest.json')
     },
     {
-        name: "firefox",
-        entry: path.join(templateDirectory, "firefox.manifest.json"),
-        output: path.join(distDirectory, "firefox", "manifest.json")
+        name: 'firefox',
+        entry: path.join(templateDirectory, 'firefox.manifest.json'),
+        output: path.join(distDirectory, 'firefox', 'manifest.json')
     }
 ];
 
-for(const manifest of manifestArray) {
+for (const manifest of manifestArray) {
     console.log(`generate ${manifest.name} manifest..`);
     const jsonManifest = JSON.parse(fs.readFileSync(manifest.entry).toString());
     const jsonOutput = Object.assign(jsonManifest, ManifestConfiguration);
-    fs.writeFileSync(manifest.output, JSON.stringify(jsonOutput, null,2));
+    fs.writeFileSync(manifest.output, JSON.stringify(jsonOutput, null, 2));
 }
