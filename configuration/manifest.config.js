6921f907eca5305c972086028215e9cb005c1128
Adrien Villalonga <nd4yza9cgmdx4cxpq4box6tv7nh80zgc@proton.me>
2024-08-28 10:37:19 +0200

    build: chrome & firefox manifest

---
+++ configuration/manifest.config.js
@@ -0,0 +1,10 @@
+
+const ManifestConfiguration = {
+
+    description: 'Cémantix/Pédantix ergonomics and features upgrade (https://cemantix.certitudes.org/)',
+    version: '3.0.0',
+    web_accessible_resources: []
+
+};
+
+export default ManifestConfiguration;
