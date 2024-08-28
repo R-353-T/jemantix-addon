a68b672dfbf55cd1b98766fc6e3acbd8e27b04fe
Adrien Villalonga <nd4yza9cgmdx4cxpq4box6tv7nh80zgc@proton.me>
2024-08-28 10:46:05 +0200

    build: add ubuntu mono font

--- configuration/manifest.config.js
+++ configuration/manifest.config.js
@@ -3,7 +3,12 @@ const ManifestConfiguration = {
 
     description: 'Cémantix/Pédantix ergonomics and features upgrade (https://cemantix.certitudes.org/)',
     version: '3.0.0',
-    web_accessible_resources: []
+    web_accessible_resources: [
+        'asset/font/ubuntu_mono/ubuntumono-bold-italic.ttf',
+        'asset/font/ubuntu_mono/ubuntumono-bold.ttf',
+        'asset/font/ubuntu_mono/ubuntumono-italic.ttf',
+        'asset/font/ubuntu_mono/ubuntumono-regular.ttf'
+    ]
 
 };
 
